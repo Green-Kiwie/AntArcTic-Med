c27a3b2211b86281cc7c2e64ccd800e3e8486703
Ashley Yee <ashleyy01535@gmail.com>
2025-04-22 22:13:36 -0700

    Added simple front page with tailwindcss confirmed working
    Signed-off-by: Ashley Yee <ashleyy01535@gmail.com>

---
+++ antarctic-med/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-5xl text-red-600">AntArcTic-Med</h1>
+          <p className="text-white">MCI development assessment through gamification</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
