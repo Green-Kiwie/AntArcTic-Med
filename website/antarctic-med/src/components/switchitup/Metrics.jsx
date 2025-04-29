b78540787cb672b6c95213f64991c9a1fed33ab6
sidsun1 <siddharthsundar2@gmail.com>
2025-04-29 16:35:47 -0700

    Added basic formatted metrics logic

--- website/antarctic-med/src/components/switchitup/Metrics.jsx
+++ website/antarctic-med/src/components/switchitup/Metrics.jsx
@@ -3,10 +3,24 @@
  */
 function sendMetrics() {}
 
+const formatMetrics = ( metrics ) => {
+    /*
+    Need to determine 3 different types
+    1. longest streak of accurate presses (measure accuracy)
+    2. number of missed presses (measures motor skills)
+    3. average reaction time (total time/number of presses)
+    */
+    return{
+        /*Will need a separate metric to be tracked in the GameField component for the longest streak*/
+        longest_streak: 0,
+        num_missed_presses: metrics.wrong_selection_missed_a_selection,
+        average_reaction_time: metrics.total_number_of_correct_selections ? (metrics.time_from_start_of_game_to_end_of_game / metrics.total_number_of_correct_selections) : 0,
+    }
+}
+
 export default function Metrics({ setGameRunning, metrics }) {
     return (
         <div>
-
             {/* Example render of metrics... could use a helper function to format metrics display */}
             <h2 className="text-2xl font-bold mb-4">Game Metrics</h2>
             <ul className="space-y-2">
@@ -19,7 +33,7 @@ export default function Metrics({ setGameRunning, metrics }) {
 
             {/* Example button to update game state */}
             <button onClick={() => setGameRunning("start")}>
-                Show Metrics
+                Play Again
             </button>
         </div>
     )
