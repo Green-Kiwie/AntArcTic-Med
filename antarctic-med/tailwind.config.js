30348957202737ab5bc7f918273b0401531a0864
Green-Kiwie <kierann.schong@gmail.com>
2025-05-09 16:11:45 -0700

    small bug fix

--- antarctic-med/tailwind.config.js
+++ antarctic-med/tailwind.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-<<<<<<< HEAD:website/antarctic-med/tailwind.config.js
-    content: [
-        './src/**/*.{js,jsx,ts,tsx}', // Make sure this matches your file paths
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-=======
   content: [
     './src/**/*.{js,jsx,ts,tsx}', // Make sure this matches your file paths
   ],
@@ -15,5 +6,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
->>>>>>> website:antarctic-med/tailwind.config.js
 }
