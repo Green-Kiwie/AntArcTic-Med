4cbd74c5e0813190ad471fc821b6d77bd186cfb0
sidsun1 <siddharthsundar2@gmail.com>
2025-05-13 21:35:55 -0700

    Added useful metrics to show metrics state

--- antarctic-med/src/components/switchitup/Metrics.jsx
+++ antarctic-med/src/components/switchitup/Metrics.jsx
@@ -1,25 +1,43 @@
+import { useEffect } from "react";
+
+
 /**
  * Sends the metrics.
  */
 function sendMetrics() {}
 
-export default function Metrics({ setGameRunning, metrics }) {
+
+export default function Metrics({ setGameRunning, metrics, setMetrics }) {
+    /*
+    Need to determine 3 different types
+    1. longest streak of accurate presses (measure accuracy)
+    2. number of missed presses (measures motor skills)
+    3. average reaction time (total time/number of presses)
+    */
+
+
+    const displayMetrics = {
+        'Longest Streak' : metrics.longest_streak || 0,
+        'Missed Presses' : metrics.wrong_selection_missed_a_selection,
+        'Average Reaction Time (in ms)' : metrics.total_number_of_correct_selections ? (metrics.time_from_start_of_game_to_end_of_game / metrics.total_number_of_correct_selections) : 0,
+    }
+
+
     return (
         <div>
-
-            {/* Example render of metrics... could use a helper function to format metrics display */}
             <h2 className="text-2xl font-bold mb-4">Game Metrics</h2>
             <ul className="space-y-2">
-                {Object.entries(metrics).map(([key, value]) => (
+                {Object.entries(displayMetrics).map(([key, value]) => (
                     <li key={key} className="text-lg">
                         <strong>{key}:</strong> {value}
                     </li>
                 ))}
             </ul>
 
+
             {/* Example button to update game state */}
             <button onClick={() => setGameRunning("start")}>
-                Show Metrics
+                Play Again
             </button>
         </div>
     )
