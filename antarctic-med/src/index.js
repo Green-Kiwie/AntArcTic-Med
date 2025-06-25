24f021e63a71cb57b10d5db033d7b65612299ff5
Green-Kiwie <kierann.schong@gmail.com>
2025-06-26 00:55:24 +1000

    allow direct access to subpages using 404.html: solution only for github pages

--- antarctic-med/src/index.js
+++ antarctic-med/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+
+//this part is for 440.html for github hosting
+const urlParams = new URLSearchParams(window.location.search);
+const redirectPath = urlParams.get('redirect');
+
+if (redirectPath) {
+  window.history.replaceState(null, '', redirectPath);
+}
+//end of 404.html part
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
