d867c27601ebd07b6d91dfdac4aad79555d40bc1
sidsun1 <siddharthsundar2@gmail.com>
2025-07-13 16:48:39 -0700

    Modified lambda to account for GET requests for the api route

--- antarctic-med/server.js
+++ antarctic-med/server.js
@@ -5,8 +5,6 @@ const app = express();
 const PORT = 5000;
 const API_GATEWAY_URL = process.env.AWS_API_GATEWAY_URL;
 
-console.log('API_GATEWAY_URL:', API_GATEWAY_URL);
-
 app.get('/api/metrics', async (req, res) => {
     const user_id = req.query.user_id;
     if(!user_id) return res.status(400).json({ error: 'user_id query parameter is required' });
