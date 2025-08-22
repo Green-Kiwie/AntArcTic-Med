505d865632ac9bac62f739aad43d84b61204fe0c
Green-Kiwie <Green-Kiwie@users.noreply.github.com>
2025-08-22 14:23:22 +0000

    deploy: 2cf144246d7fd9f197594dace725f5a2f9c38e55

--- antarctic-med/src/game/matchitup/GameHelpers/EndGame.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export function EndGameButton(context){
-    //return a button that calls the endGame function on click
-}
-
-export function endGame(){
-    //changes the setGameRunning in context to "metrics"
-}
